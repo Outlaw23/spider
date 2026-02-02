44e357cd574e7816918ef3624c1a3d14a062527c
Outlaw23 <127243391+Outlaw23@users.noreply.github.com>
2026-02-02 05:51:38 +0100

    the description for assignment 3
    I forgot to describe assignment 3, now I have added the description

--- src/main/java/org/example/Spider/models/Components/Sub_Screens/Components_Picture_Screen/Picture_Explanation_Component.java
+++ src/main/java/org/example/Spider/models/Components/Sub_Screens/Components_Picture_Screen/Picture_Explanation_Component.java
@@ -22,9 +22,23 @@ public class Picture_Explanation_Component {
 	 */
 	public static JLabel ExplanationPicture() {
 		MasterLabel explanation = new MasterLabel(
-			"coming soon",
-				white
-		);
+				"<html>" +
+						"<font size='26'><b>Assignment 3</b></font><br/><br/>" +
+
+						"This assignment has two phases and uses images.<br/><br/>" +
+
+						"<b>Phase 1:</b><br/>" +
+						"The user sees an image.<br/>" +
+						"The user writes the objects from the image in Hado.<br/>" +
+						"The user writes a short description in English.<br/><br/>" +
+
+
+						"<b>Phase 2:</b><br/>" +
+						"The application checks the answers.<br/>" +
+						"The user chooses correct or incorrect based on how similar the descriptions are.<br/>" +
+						"The final score is shown." +
+						"</html>",white);
+
 		return explanation.getMasterLabel();
 	}
 
